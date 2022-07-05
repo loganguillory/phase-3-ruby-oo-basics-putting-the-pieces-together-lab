eaaea7a000aa8b27cccde6d2fb07f702d1081601
Logan Guillory <97050508+loganguillory@users.noreply.github.com>
2022-07-04 21:05:40 -0400

    commit tree bf3de5441bd3da708f3d692badfcb456a217b23c

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,30 @@
+class Book
+
+
+attr_reader :title
+attr_accessor :author, :page_count, :genre
+
+    
+    def initialize(title)
+        @title = title
+    end 
+
+    # def author(author)
+    #     @author = author
+    # end 
+    def turn_page
+    puts "Flipping the page...wow, you read fast!"
+    end
+end 
+
+# book_title = Book.new("And Then There Were None")
+# book_title.title
+
+
+
+
+
+
+
 
 
